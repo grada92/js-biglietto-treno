fec90ddcf740f67cf18114d4aa5ec63963de535c
grada92 <danielegradassai@gmail.com>
2022-08-23 14:48:30 +0200

    inizio javascript

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-console.log("js ok!");
 
-console.log('JS OK!');
+const text = prompt ('quanti anni hai?');
+const textAsNumber = parseInt(text);
+
+console.log(textAsNumber);
+
+const km = prompt ('quanti km vuoi percorrere?')
+const number = parseInt(km);
+
+console.log(km);
+
+
+
+
+
+if (text < '18') {
+
+} console.log ()
 
 
